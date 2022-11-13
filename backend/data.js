35df2341d8ef9a38c14a848c9295e7f4171ea5bb
manaressam9~ <manareldredy@gmail.com>
2022-11-14 00:36:09 +0200

    fetch data from node server

--- backend/data.js
+++ backend/data.js
@@ -50,7 +50,7 @@ const data = {
     },
     {
       name: 'Nike slim shirt',
-      slug: 'nike-slim-shirt',
+      slug: 'nike-slim-shirt1',
       category: 'shirts',
       image: '/images/p1.png',
       price: 120,
@@ -62,7 +62,7 @@ const data = {
     },
     {
       name: 'Nike slim pants',
-      slug: 'nike-slim-pants',
+      slug: 'nike-slim-pants1',
       category: 'pants',
       image: '/images/p2.png',
       price: 120,
@@ -74,7 +74,7 @@ const data = {
     },
     {
       name: 'Nike wide pants',
-      slug: 'nike-wide-pants',
+      slug: 'nike-wide-pants1',
       category: 'pants',
       image: '/images/p3.png',
       price: 150,
@@ -86,7 +86,7 @@ const data = {
     },
     {
       name: 'Addidas wide pants',
-      slug: 'Addidas-wide-pants',
+      slug: 'Addidas-wide-pants1',
       category: 'pants',
       image: '/images/p4.png',
       price: 170,
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//import data from '../data';
 export default function HomeScreen() {
+  // define a state "products" to save products from backend
+  // useState hook > returns a variable and function to update this variable
+  const [products, setProducts] = useState([]);
+  // function inside useEffect is run one time after rendering the component
+  // call axios.get to send ajax req to /api/products address and then save result in "result" var
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
